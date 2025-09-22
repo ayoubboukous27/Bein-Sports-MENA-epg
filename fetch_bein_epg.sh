42fa77af7f8d31e3ab2911b1f1c0e28951720107
ayoubboukous27 <37737944+ayoubboukous27@users.noreply.github.com>
2025-09-22 11:09:42 +0100

    Create fetch_bein_epg.sh

---
+++ fetch_bein_epg.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# 1) سحب مشروع iptv-org/epg
+if [ ! -d "epg" ]; then
+  git clone --depth 1 https://github.com/iptv-org/epg.git
+fi
+
+cd epg
+
+# 2) تثبيت المتطلبات
+npm install
+
+# 3) تشغيل الجلب لموقع beIN (غيّر الاسم لو مختلف)
+npm run grab --- --site=beinsports.com --output=guide_bein.xml --days=2
+
+# 4) رجوع للمجلد الأصلي
+cd ..
+
+# 5) تشغيل سكربت Python لتحويل IDs
+python3 map_bein_ids.py epg/guide_bein.xml epg_mapped.xml
